dc82cb00752259da017c401f48e251b3832984c9
yutaroon <y-nishi@adventure-inc.co.jp>
2021-05-29 16:01:31 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yuta-ron/sql-comp
+
+go 1.16
+
+require github.com/blastrain/vitess-sqlparser v0.0.0-20201030050434-a139afbb1aba // indirect
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+ 	"fmt"
+	"github.com/blastrain/vitess-sqlparser/sqlparser"
+)
+
+func main() {
+	stmt, err := sqlparser.Parse("select * from user_items where user_id=1 order by created_at limit 3 offset 10")
+	if err != nil {
+		panic(err)
+	}
+	
+///	fmt.Printf("stmt = %+v\n", stmt)
+
+	fmt.Println(len(stmt))
+}
