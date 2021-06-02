beaa4a795502c1c68736eb54c8de0ca2dbc4cb6e
yutaroon <y-nishi@adventure-inc.co.jp>
2021-06-02 23:08:05 +0900

    fix database definition

--- mysql/db1.initdb.d/db1.initdb.sql
+++ mysql/db1.initdb.d/db1.initdb.sql
@@ -1,1 +1,2 @@
-create table mydb.user (id int, name varchar(10));
+create table mydb.user (id int, name varchar(10), salary int, address varchar(10));
+create table mydb.job (id int, job varchar(10));
--- mysql/db2.initdb.d/db2.initdb.sql
+++ mysql/db2.initdb.d/db2.initdb.sql
@@ -1,1 +1,2 @@
-create table mydb.user (id int, name varchar(15) not null, job varchar(10));
+create table mydb.user (id int, name varchar(15) not null, job varchar(10), address char(10));
+create table mydb.rank (id int, rank varchar(100));
